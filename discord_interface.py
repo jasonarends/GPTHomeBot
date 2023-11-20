21ab9fddb9c644f2354e24fd86900e89f2d3d4ee
jason <jason.arends@gmail.com>
2023-11-20 06:51:40 -0600

    basic working version

--- discord_interface.py
+++ discord_interface.py
@@ -1,70 +1,44 @@
 import discord
+from openai_interface import OpenAIAssistant
 import os
-import datetime
-from icecream import ic
 from dotenv import load_dotenv
+from icecream import ic
 
 class DiscordInterface:
-    """
-    Handles interactions with Discord.
-    """
-
-    def __init__(self, assistant):
-        """
-        Initializes the Discord bot.
-        """
+    def __init__(self):
         load_dotenv()
         self.token = os.getenv('DISCORD_BOT_TOKEN')
-
-        # Set up Intents
-        intents = discord.Intents.default()
+        self.assistant_id = os.getenv('OPENAI_ASSISTANT_ID')
+        self.assistant = OpenAIAssistant(assistant_id=self.assistant_id)
         
+        intents = discord.Intents.default()
         intents.messages = True  # Adjust this based on your needs
         intents.message_content = True
         intents.guild_messages = True
         intents.dm_messages = True
 
         self.client = discord.Client(intents=intents)
-        self.assistant = assistant
 
     async def on_ready(self):
-        """
-        Called when the bot is ready to start.
-        """
         print(f'{self.client.user} has connected to Discord!')
 
     async def on_message(self, message):
-        """
-        Handles incoming messages.
-        """
         if message.author == self.client.user:
             return
 
-        # Debugging: Print the message content
-        ic(message)
-        ic(message.content)
-        ic(message.mentions)
 
-        # Improved mention detection
         if self.client.user in message.mentions and isinstance(message.channel, discord.TextChannel):
-            response, thread_id = self.assistant.handle_message(None, message.content)
+            responses, thread_id = await self.assistant.handle_message(None, message.content)
             thread = await message.create_thread(name=thread_id)
-            await thread.send(response)
-            # Create a unique thread name
-            #timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-            #thread_name = f"GPT Chat - {timestamp}"
-            #thread = await message.create_thread(name=thread_name)
-            #await thread.send(f"Thread started in response to {message.author.display_name}'s mention.")
+            for response in responses:
+                await thread.send(response)
 
-        # Respond to messages in threads started by the bot
         elif isinstance(message.channel, discord.Thread) and message.channel.name.startswith("thread_"):
-            response, _ = self.assistant.handle_message(message.channel.name, message.content)
-            await message.channel.send(response)
+            responses, _ = await self.assistant.handle_message(message.channel.name, message.content)
+            for response in responses:
+                await message.channel.send(response)
 
     def run(self):
-        """
-        Starts the Discord bot.
-        """
         self.client.event(self.on_ready)
         self.client.event(self.on_message)
         self.client.run(self.token)
--- openai_interface.py
+++ openai_interface.py
@@ -1,9 +1,9 @@
 from openai import OpenAI
-from openai.types.beta import Thread
-from openai.types.
 from dotenv import load_dotenv
+import asyncio
+from icecream import ic
 import os
-
+import time
 
 class OpenAIAssistant:
     def __init__(self, assistant_id, model="gpt-4-1106-preview"):
@@ -13,47 +13,58 @@ class OpenAIAssistant:
         self.client = OpenAI(api_key=self.apikey)
         self.assistant_id = assistant_id
         self.model = model
-        self.thread_id = None
-        
 
-    def create_thread(self, init_message):
+    def create_thread(self):
         # Create a new thread
-        
+        ic()
         thread = self.client.beta.threads.create()
-        self.thread_id = thread.id
-        return self.thread_id
+        return thread.id
 
-    def add_message(self, thread, role, content):
+    def add_message(self, thread_id, role, content):
         # Add a message to the thread
+        ic()
+        ic(thread_id, role, content)
         self.client.beta.threads.messages.create(
-            thread_id=self.thread_id,
+            thread_id=thread_id,
             role=role,
             content=content
         )
         
-    def handle_message(self, thread_id, content):
+    async def handle_message(self, thread_id, content):
         if not thread_id:
-            thread = self.client.beta.threads.create()
-            thread_id = thread.id
-            self.client.beta.threads.messages.create(thread_id=thread_id, role="user", content=content)
-            response = self.run_assistant(thread_id)
-        else:
-            self.client.beta.threads.messages.create(thread_id=thread_id, role="user", content=content)
-            response = self.run_assistant(thread_id)
+            thread_id = self.create_thread()
+        ic()
+        self.add_message(thread_id, "user", content)
+        return await self.run_assistant(thread_id), thread_id
 
-        return response, thread_id
-
-
-    def run_assistant(self):
-        # Run the assistant on the thread
+    async def run_assistant(self, thread_id):
+        # Initiate a run on the thread
         run = self.client.beta.threads.runs.create(
-            thread_id=self.thread_id,
+            thread_id=thread_id,
             assistant_id=self.assistant_id
         )
-        return run
+        run_id = run.id  # Store the run_id
 
-# Example usage
-# assistant = OpenAIAssistant(assistant_id='your_assistant_id')
-# thread_id = assistant.create_thread()
-# assistant.add_message("user", "Hello, how are you?")
-# run = assistant.run_assistant()
+        # Check the run status periodically
+        while True:
+            run_status = self.client.beta.threads.runs.retrieve(
+                thread_id=thread_id,
+                run_id=run_id
+            )
+            if run_status.status == 'completed':
+                break
+            await asyncio.sleep(1)
+
+        # Retrieve the thread to get messages
+        messages = self.client.beta.threads.messages.list(
+            thread_id=thread_id
+        )
+        ic(messages)
+
+        # Filter the assistant's responses for this run and sort by creation time
+        assistant_responses = [msg for msg in messages.data if msg.role == "assistant" and msg.run_id == run_id]
+        sorted_responses = sorted(assistant_responses, key=lambda msg: msg.created_at)
+
+        # Extract text values from sorted responses
+        return [response.content[0].text.value for response in sorted_responses if isinstance(response.content, list) and response.content]
+
